603536615a23a0204b076dec69180d9cb1602c84
Sualeh Siddiqui <sualehmsiddiqui@gmail.com>
2023-01-29 21:09:13 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Question #01
+// var greet =("hello, Welcome to my website")
+// alert(greet);
+
+// Question #02
+// var wrongpassword =("Error! Please enter a valid password");
+// alert (wrongpassword);
+
+// Question #03
+// var wlcm = ("Welcome to JS Land... \nHappy Coding!"); 
+// alert (wlcm);
+
+// Question #04
+// var wlcm =("Welcome to JS Land....");
+// var grt =("Happy Coding!");
+// alert (wlcm);
+// alert (grt);
+
+// Question #05
+// alert ("Hello ... Ican run JS through My web browser's concole");
+
+// Question #07 on index page
+
+
